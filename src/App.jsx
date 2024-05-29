e42b4d6a1ad6e1c85827aab9324265703d6604cc
JasminA-97 <21mp1828@rit.ac.in>
2024-05-29 11:46:13 +0530

    Project Fair

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Auth from './pages/Auth'
+import Dashboar from './pages/Dashboar'
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Auth/>}/>
+        <Route path='/register' element={<Auth insideRegister={true}/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/dashboard' element={<Dashboar/>}/>
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Add.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
+import addImg from '../assets/AddImg.png'
+
+const Add = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+    <button onClick={handleShow} className="btn btn-primary"><i className="fa-solid fa-plus"></i> New Project</button>
+     <Modal size='lg' centered show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Project Details!!!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row align-items-center">
+            <div className="col-lg-4">
+              <label>
+                <input type="file" style={{display:'none'}}/>
+                <img height={'200px'} className='img-fluid' src={addImg} alt="" />
+              </label>
+            </div>
+            <div className="col-lg-8">
+              <div className="mb-2">
+                <input type="text" className='form-control' placeholder='Project Title'/>
+              </div>
+              <div className="mb-2">
+                <input type="text" className='form-control' placeholder='Languages Used In Project'/>
+              </div>
+              <div className="mb-2">
+                <input type="text" className='form-control' placeholder='Project GitHub Link'/>
+              </div>
+              <div className="mb-2">
+                <input type="text" className='form-control' placeholder='Project Website Link'/>
+              </div>
+            </div>
+          </div>
+
+          <div>
+          <input type="text" className='form-control' placeholder='Project Review'/>
+          </div>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary">Add</Button>
+        </Modal.Footer>
+     </Modal>
+    </>
+  )
+}
+
+export default Add
